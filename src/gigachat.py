a8967e43772e57acaa8a217ee739b68c26bf6f45
Alexander Evdokimov <83905079+snakerzr@users.noreply.github.com>
2024-04-25 23:03:03 +0300

    welp

--- src/gigachat.py
+++ src/gigachat.py
@@ -12,38 +12,38 @@ CREDS = os.environ["CREDS"]
 chat = GigaChat(credentials=CREDS, scope="GIGACHAT_API_CORP", verify_ssl_certs=False)
 
 
-def compare_summarize(old_article, new_article):
-    messages = [
-        SystemMessage(content="Ты юрист, который хорошо разбирается в текстах законов.")
-    ]
-    messages.append(
-        HumanMessage(
-            content=f"""
-    Привет! Сравни две итерации фрагмента закона и опиши юридическим языком коротко тезисно изменения:
-    Старая версия закона: {old_article}
-    Новая версия закона: {new_article}
-    ОЧЕНЬ КОРОТКО, МАЛО СЛОВ, КРАТКО ОБОБЩИ
-    """
-        )
-    )
+def compare_summarize(old_article, new_article, system_message, instruction):
+    if not system_message:
+        system_message = "Ты юрист, который хорошо разбирается в текстах законов."
+
+    if not instruction:
+        instruction = f"""
+Привет! Сравни две итерации фрагмента закона и опиши юридическим языком коротко тезисно изменения:
+Старая версия закона: {old_article}
+Новая версия закона: {new_article}
+ОЧЕНЬ КОРОТКО, МАЛО СЛОВ, КРАТКО ОБОБЩИ
+    """.strip()
+
+    messages = [SystemMessage(content=system_message)]
+    messages.append(HumanMessage(content=instruction))
 
     res = chat(messages)
 
     return res.content
 
-def generate_analysis(query,expertise):
-    messages = [
-        SystemMessage(
-            content="Ты юрист, который хорошо разбирается в текстах законов."
-        )
-    ]
-    messages.append(HumanMessage(content=f'''
-    Напиши экспертно-правовой комментарий об изменениях в статье закона, основываясь на выдержках из научной работы.
-    Изменения в законе: {query}
-    Выдержки из научной работы: {expertise}
-    В ответе делай отсылку к научной работе. Ответ юридическим языком:
-    '''
-                                ))
+
+def generate_analysis(query, expertise, system_message, instruction):
+    if not system_message:
+        system_message = "Ты юрист, который хорошо разбирается в текстах законов."
+
+    if not instruction:
+        instruction = f"""
+Напиши экспертно-правовой комментарий об изменениях в статье закона, основываясь на выдержках из научной работы.
+Изменения в законе: {query}
+Выдержки из научной работы: {expertise}
+В ответе делай отсылку к научной работе. Ответ юридическим языком:""".strip()
+    messages = [SystemMessage(content=system_message)]
+    messages.append(HumanMessage(content=instruction))
 
     res = chat(messages)
     return res.content
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,21 @@ from src.FAISS import faiss_db
 from src.comparison import splitting_wrapped
 from src.gigachat import compare_summarize, generate_analysis
 
-
 def main():
     st.title("Загрузка текстовых файлов")
 
     law_old = st.file_uploader("Загрузите старую редакцию", type=["txt"])
     law_actual = st.file_uploader("Загрузите новую редакцию", type=["txt"])
 
-    if law_old is not None and law_actual is not None:
+    system_message_1 = st.text_input("Введите системное сообщение для сравнения редакций:")
+    instruction_1 = st.text_input("Введите инструкцию для сравнения редакций:")
+
+    system_message_2 = st.text_input("Введите системное сообщение для экспертного анализа:")
+    instruction_2 = st.text_input("Введите инструкцию для экспертного анализа:")
+
+    process_button = st.button("Обработать данные")
+
+    if process_button and law_old is not None and law_actual is not None:
         law_old = law_old.read().decode("cp1251")
         law_actual = law_actual.read().decode("cp1251")
 
@@ -21,22 +28,20 @@ def main():
             for _, row in df.iterrows():
                 article_old = row['Старая редакция']
                 article_new = row['Новая редакция']
-                result.append(compare_summarize(article_old, article_new))
+                result.append(compare_summarize(article_old, article_new, system_message_1, instruction_1))
             st.success('Завершено!')
 
+        st.write(result)
 
         final_answer = ''
         with st.spinner('Подготовка экспертного мнения...'):
             for query in result:
                 expertise = faiss_db.similarity_search_with_score(query, k=5)
-                answer = generate_analysis(query,expertise)
+                answer = generate_analysis(query, expertise, system_message_2, instruction_2)
                 final_answer += answer + '\n\n'
             st.success('Завершено!')
 
         st.write(final_answer)
-
-
-    
 
 if __name__ == "__main__":
     main()
